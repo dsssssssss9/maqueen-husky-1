1b38ce0ce316e65acbab9025d2f595a13551c85d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-01-09 18:32:16 +0000

    check HUSKY & Logic using LED on MaQueen

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
+input.onButtonPressed(Button.B, function () {
+    Move = false
+})
+input.onButtonPressed(Button.A, function () {
+    Move = true
+})
+let Move = false
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
 maqueen.motorStop(maqueen.Motors.All)
 basic.clearScreen()
+Move = false
 basic.showIcon(IconNames.StickFigure)
 basic.forever(function () {
-	
+    huskylens.request()
+    if (Move) {
+        basic.showIcon(IconNames.Yes)
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        } else {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        }
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.showIcon(IconNames.No)
+    }
 })
