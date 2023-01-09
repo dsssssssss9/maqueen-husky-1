4188e6f3de74b2d6749b5af5ca4a1cac49eac583
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-01-09 17:44:34 +0000

    stop all motors & Init HUSKY

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
+maqueen.motorStop(maqueen.Motors.All)
+basic.clearScreen()
+basic.showIcon(IconNames.StickFigure)
 basic.forever(function () {
 	
 })
